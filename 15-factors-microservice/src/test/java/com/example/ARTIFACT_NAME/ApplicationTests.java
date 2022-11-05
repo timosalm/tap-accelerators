3d316b72fed8ddef058bc4254c8a762a2338fc3b
Timo Salm <tsalm@vmware.com>
2022-11-06 00:25:04 +0100

    Add 15-factors-microservice accelerator

--- 15-factors-microservice/src/test/java/com/example/ARTIFACT_NAME/ApplicationTests.java
+++ 15-factors-microservice/src/test/java/com/example/ARTIFACT_NAME/ApplicationTests.java
@@ -3,10 +3,16 @@ package com.example.ARTIFACT_NAME;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @TestPropertySource(properties = "management.metrics.export.wavefront.enabled=false")
 @SpringBootTest
 class ApplicationTests {
+
+	@MockBean
+  private JwtDecoder jwtDecoder;
+
 
 	@Test
 	void contextLoads() {
