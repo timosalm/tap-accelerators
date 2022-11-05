d72252e4761af14f89edf92aa2e636eeb18107a4
Timo Salm <tsalm@vmware.com>
2022-11-06 00:43:39 +0100

    Add 15-factors-microservice accelerator

--- 15-factors-microservice/src/main/java/com/example/ARTIFACT_NAME/HelloWorldResource.java
+++ 15-factors-microservice/src/main/java/com/example/ARTIFACT_NAME/HelloWorldResource.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(HelloWorldResource.BASE_URI)
 public class HelloWorldResource {
   
   static final String BASE_URI = "/api/v1/helloworld";
