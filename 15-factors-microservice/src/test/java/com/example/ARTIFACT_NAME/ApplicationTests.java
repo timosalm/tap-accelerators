f08d3a3dc96344b27513e12c9483713bed5e0560
Timo Salm <52704922+tsalm-pivotal@users.noreply.github.com>
2023-02-15 16:50:26 +0100

    Update ApplicationTests.java

--- 15-factors-microservice/src/test/java/com/example/ARTIFACT_NAME/ApplicationTests.java
+++ 15-factors-microservice/src/test/java/com/example/ARTIFACT_NAME/ApplicationTests.java
@@ -4,17 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @TestPropertySource(properties = "management.metrics.export.wavefront.enabled=false")
 @SpringBootTest
 class ApplicationTests {
 
-	@MockBean
-  private JwtDecoder jwtDecoder;
-
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() { }
 
 }
